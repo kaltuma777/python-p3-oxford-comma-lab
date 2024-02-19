4e3ffd9c8a10350282a98facde34016e04069de6
Kaltuma <Kaltuma.abdi@student.moringaschool.com>
2024-02-19 22:44:30 +0300

    Completed Assign

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    elif len(items) >= 3:
+        formatted = ', '.join(items[:-1])
+        return f"{formatted}, and {items[-1]}"
+
+print(oxford_comma(['one','two','three','four']))
